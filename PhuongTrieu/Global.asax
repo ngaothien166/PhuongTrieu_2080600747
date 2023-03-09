43bf7f16517b977dfeea66df5871c5f9f5ab09b3
ADMIN <ADMIN@DESKTOP-DJ4UJTR>
2023-03-09 23:42:09 +0700

    Add project files.

---
+++ PhuongTrieu/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="PhuongTrieu.MvcApplication" Language="C#" %>
---
+++ PhuongTrieu/Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+﻿namespace PhuongTrieu.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<PhuongTrieu.Models.ApplicationDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(PhuongTrieu.Models.ApplicationDbContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
